1b428e47b64561917f7c1d9330f353520c976ebf
Samuel Dost <samuel.dost@gmail.com>
2015-11-26 15:58:01 -0500

    Updated to use supervisor.

--- sequence/lib/sequence.ex
+++ sequence/lib/sequence.ex
@@ -1,2 +1,14 @@
 defmodule Sequence do
+    use Application
+
+    def start(_type, _args) do
+        import Supervisor.Spec, warn: false
+
+        children = [
+            worker(Sequence.Server, [123])
+        ]
+
+        opts = [strategy: :one_for_one, name: Sequence.Supervisor]
+        {:ok, _pid} = Supervisor.start_link(children, opts)
+    end
 end
