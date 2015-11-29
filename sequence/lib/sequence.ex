0274223e1c0d3689ec70a2a8bc9ac042ed1baf65
Samuel Dost <samuel.dost@gmail.com>
2015-11-28 23:08:56 -0500

    Added supervisors with stash.

--- sequence/lib/sequence.ex
+++ sequence/lib/sequence.ex
@@ -2,13 +2,6 @@ defmodule Sequence do
     use Application
 
     def start(_type, _args) do
-        import Supervisor.Spec, warn: false
-
-        children = [
-            worker(Sequence.Server, [123])
-        ]
-
-        opts = [strategy: :one_for_one, name: Sequence.Supervisor]
-        {:ok, _pid} = Supervisor.start_link(children, opts)
+        {:ok, _pid} = Sequence.Supervisor.start_link(123)
     end
 end
